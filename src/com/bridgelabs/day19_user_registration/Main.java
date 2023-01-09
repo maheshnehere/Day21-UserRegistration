777358528875cfa2df1e750ec5e3b7db7ed00330
Sourav2707 <smartsourav33@gmail.com>
2023-01-09 19:07:59 +0530

    UC1 Validating first name

--- src/com/bridgelabs/day19_user_registration/Main.java
+++ src/com/bridgelabs/day19_user_registration/Main.java
@@ -1,7 +1,15 @@
 package com.bridgelabs.day19_user_registration;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to day 19 user registration program");
+        UserRegistration user = new UserRegistration();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first name to validate");
+        String firstName = scanner.next();
+        if (user.Validation(firstName) == true)
+            System.out.println("Valid input first name");
+        else
+            System.out.println("Invalid input first name");
     }
 }
---
+++ src/com/bridgelabs/day19_user_registration/UserRegistration.java
@@ -0,0 +1,12 @@
+package com.bridgelabs.day19_user_registration;
+
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+        public boolean Validation(String firstName)
+        {
+            Pattern pattern=Pattern.compile("^[A-Z][a-zA-Z]{2,}");
+            boolean matches = pattern.matcher(firstName).matches();
+            return matches;
+        }
+}
