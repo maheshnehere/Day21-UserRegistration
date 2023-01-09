1025f3c2cbe17c3dce898f19d16ae0306250d8f3
Sourav2707 <smartsourav33@gmail.com>
2023-01-09 19:26:50 +0530

    UC6 validating password and it should starts with caps

--- src/com/bridgelabs/day19_user_registration/UserRegistration.java
+++ src/com/bridgelabs/day19_user_registration/UserRegistration.java
@@ -24,7 +24,7 @@ public class UserRegistration {
         return matches;
     }
     public boolean validatePassword(String password) {
-        Pattern pattern = Pattern.compile("[a-z]{8}");
+        Pattern pattern = Pattern.compile("(?=.*[A-Z])[a-zA-Z]{8}");
         boolean matches = pattern.matcher(password).matches();
         return matches;
     }
