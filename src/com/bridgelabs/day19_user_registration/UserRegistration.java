2a5a1b233343d3aa1ff98492a3feff15d1258e27
maheshnehere <maheshnehere93@gmail.com>
2023-04-24 19:19:48 +0530

    Update UserRegistration.java

--- src/com/bridgelabs/day19_user_registration/UserRegistration.java
+++ src/com/bridgelabs/day19_user_registration/UserRegistration.java
@@ -2,7 +2,7 @@ package com.bridgelabs.day19_user_registration;
 
 import java.util.regex.Pattern;
 
-public class UserRegistration {
+public class UserRegistration { 
         public boolean validateName(String name)
         {
             Pattern pattern=Pattern.compile("^[A-Z][a-zA-Z]{2,}");
