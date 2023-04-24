24237195853e09a8c722f68a2bd5673e9b05e9fe
maheshnehere <maheshnehere93@gmail.com>
2023-04-24 19:01:32 +0530

    Update Main.java

--- src/com/bridgelabs/day19_user_registration/Main.java
+++ src/com/bridgelabs/day19_user_registration/Main.java
@@ -1,5 +1,6 @@
 package com.bridgelabs.day19_user_registration;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to day 19 user registration program");
