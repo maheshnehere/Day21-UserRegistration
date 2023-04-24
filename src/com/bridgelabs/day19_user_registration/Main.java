e6fe3bfa76769075cff856bb92bf8f41031f4e3e
maheshnehere <maheshnehere93@gmail.com>
2023-04-24 18:41:44 +0530

    Update Main.java

--- src/com/bridgelabs/day19_user_registration/Main.java
+++ src/com/bridgelabs/day19_user_registration/Main.java
@@ -45,5 +45,6 @@ public class Main {
         SampleEmailCheck sampleEmailCheck = new SampleEmailCheck();
         sampleEmailCheck.validEmailcheck();
         sampleEmailCheck.invalidEmailcheck();
+        
     }
 }
